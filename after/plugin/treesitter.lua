29e4329ad3b48044e4a7b6f9d3a2f2b30b860778
jborkows <jacek.borkowskiit@gmail.com>
2023-07-03 00:51:54 +0200

    too fast

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,7 +1,7 @@
 require 'nvim-treesitter.configs'.setup {
 	-- A list of parser names, or "all"
 	ensure_installed = { "c", "help", "lua", "rust", "bash", "css", "dockerfile", "go", "java", "javascript", "python",
-		"rust", "sql", "typescript", "yaml", "vim", "ansiblels" },
+		"rust", "sql", "typescript", "yaml", "vim" },
 
 	-- Install parsers synchronously (only applied to `ensure_installed`)
 	sync_install = false,
