8cad273318224ce1e81870b16f82344cb78d2288
jborkows <jacek.borkowskiit@gmail.com>
2023-07-08 00:11:11 +0200

    add replace word

--- lua/jborkows/remap.lua
+++ lua/jborkows/remap.lua
@@ -34,3 +34,6 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 -- make file executable
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+
+-- replace word
+vim.keymap.set("n", "<leader>rw", '"_dwp', { desc = "try to replace word" })
