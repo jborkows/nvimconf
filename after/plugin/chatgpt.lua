0e567de26c97e080f22375d072f71e83190fef3f
jborkows <jacek.borkowskiit@gmail.com>
2023-09-24 21:18:10 +0200

    optional chat gpt - lua function

--- after/plugin/chatgpt.lua
+++ after/plugin/chatgpt.lua
@@ -1,6 +1,14 @@
--- require("chatgpt").setup(
--- 	{
+function EnableChat()
+	-- local result = vim.fn.system( "op read op://private/OpenAI/credential --no-newline")
+	vim.env.OPENAI_API_HOST = "api.openai.com"
 
--- 		api_key_cmd = "op read op://private/OpenAI/credential --no-newline"
--- 	}
--- )
+	require("chatgpt").setup(
+		{
+
+			api_key_cmd = "op read op://private/OpenAI/credential --no-newline",
+			openai_params = {
+				model = "gpt-3.5-turbo",
+			}
+		}
+	)
+end
