b8747e2f8f4eb266119573fad2d60436bc0a81df
jborkows <jacek.borkowskiit@gmail.com>
2023-09-24 21:16:50 +0200

    harpoon use lua functions + shortcuts

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,5 +1,7 @@
-local mark= require("harpoon.mark")
-local ui=require("harpoon.ui")
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
 
-vim.keymap.set('n',"<leader>a", "<cmd>lua require('harpoon.mark').add_file()<cr>")
-vim.keymap.set("n","<C-e>", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>")
+vim.keymap.set('n', "<leader>a", function() mark.add_file() end)
+vim.keymap.set("n", "<C-e>", function() ui.toggle_quick_menu() end)
+vim.keymap.set("n", "<A-2>", function() ui.nav_next() end)
+vim.keymap.set("n", "<A-1>", function() ui.nav_prev() end)
