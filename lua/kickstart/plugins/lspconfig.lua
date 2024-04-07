956635c9dc4e400c10cfb1b2b10f8495037c2788
jborkows <jacek.borkowskiit@gmail.com>
2024-04-07 10:55:01 +0200

    Update lazy + Mason. Add docker and python lsp.

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -81,7 +81,8 @@ return {
             unusedparams = true,
           },
         },
-        -- pyright = {},
+        pyright = {},
+        dockerls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
