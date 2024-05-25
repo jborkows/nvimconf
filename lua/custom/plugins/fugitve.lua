2a46c9a8b10434635748005671c291061da8cb75
jborkows <jacek.borkowskiit@gmail.com>
2024-05-25 12:53:15 +0200

    Git fugitive

--- lua/custom/plugins/fugitve.lua
+++ lua/custom/plugins/fugitve.lua
@@ -5,9 +5,8 @@ return {
       vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
       vim.keymap.set('n', 'gr', '<cmd>diffget //3<CR>')
       vim.keymap.set('n', 'gl', '<cmd>diffget //2<CR>')
-      vim.keymap.set('n', '<leader>gh', '<cmd>OGcLog<CR>',
-        { desc = 'open file history and populate quick list with file version' })
-      vim.keymap.set('n', '<leader>gd', '<cmd>Gdiffsplit<CR>', { desc = 'dif split' })
+      vim.keymap.set('n', '<leader>gh', '<cmd>0GcLog<CR>')
+      vim.keymap.set('n', '<leader>gd', '<cmd>Gdiffsplit<CR>')
     end,
   },
 }
