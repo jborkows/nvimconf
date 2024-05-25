2075cedadf5b81a2d065c0d6a3c1df465247feeb
jborkows <jacek.borkowskiit@gmail.com>
2024-05-25 12:45:44 +0200

    Add git diffs in fugitive

--- lua/custom/plugins/fugitve.lua
+++ lua/custom/plugins/fugitve.lua
@@ -5,6 +5,9 @@ return {
       vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
       vim.keymap.set('n', 'gr', '<cmd>diffget //3<CR>')
       vim.keymap.set('n', 'gl', '<cmd>diffget //2<CR>')
+      vim.keymap.set('n', 'gh', '<cmd>OGcLog<CR>',
+        { desc = 'open file history and populate quick list with file version' })
+      vim.keymap.set('n', 'gd', '<cmd>Gdiffsplit<CR>', { desc = 'dif split' })
     end,
   },
 }
