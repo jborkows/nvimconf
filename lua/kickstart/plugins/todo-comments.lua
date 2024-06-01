70cd6aabc64723c9f3499cd97646eb9caf769a3a
jborkows <jacek.borkowskiit@gmail.com>
2024-06-02 01:48:11 +0200

    Trouble nvim

--- lua/kickstart/plugins/todo-comments.lua
+++ lua/kickstart/plugins/todo-comments.lua
@@ -1,5 +1,25 @@
 -- Highlight todo, notes, etc in comments
 return {
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+  {
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = { signs = true },
+    init = function()
+      require('todo-comments').setup {
+        keywords = {
+          FIX = {
+            icon = ' ', -- icon used for the sign, and in search results
+            color = 'error', -- can be a hex color, or a named color (see below)
+            alt = { 'FIXME', 'BUG', 'FIXIT', 'ISSUE' }, -- a set of other keywords that all map to this FIX keywords
+            -- signs = false, -- configure signs for some keywords individually
+          },
+          TODO = { icon = ' ', color = 'info' },
+          HACK = { icon = ' ', color = 'warning' },
+        },
+        -- more configuration options here
+      }
+    end,
+  },
 }
 -- vim: ts=2 sts=2 sw=2 et
