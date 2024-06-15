9949ce05122f036c2b6106023b90d37dd1753210
jborkows <jacek.borkowskiit@gmail.com>
2024-06-16 01:16:23 +0200

    Chatgpt one key to control all

--- lua/custom/plugins/chatgpt.lua
+++ lua/custom/plugins/chatgpt.lua
@@ -1,3 +1,4 @@
+local initialized = false
 return {
   -- Lazy
   {
@@ -6,7 +7,7 @@ return {
     config = function()
       vim.env.OPENAI_API_HOST = 'api.openai.com'
 
-      vim.api.nvim_create_user_command('EnableChat', function()
+      local function initialize()
         require('chatgpt').setup {
           api_key_cmd = 'op read op://private/OpenAI/credential --no-newline',
           openai_params = {
@@ -14,7 +15,18 @@ return {
             max_tokens = 4096,
           },
         }
-      end, {})
+        initialized = true
+      end
+
+      vim.api.nvim_create_user_command('EnableChat', initialize, {})
+
+      local function openChat()
+        if not initialized then
+          initialize()
+        end
+        vim.cmd.ChatGPT()
+      end
+      vim.keymap.set('n', '<F2>', openChat, { desc = 'open chat' })
     end,
     dependencies = {
       'MunifTanjim/nui.nvim',
