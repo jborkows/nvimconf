f94a586c36bc25791ba51aa9eb516d2f53036a2a
jborkows <jacek.borkowskiit@gmail.com>
2024-09-12 20:24:21 +0200

    Fugitive populate quick fix with history of file

--- lua/custom/plugins/fugitve.lua
+++ lua/custom/plugins/fugitve.lua
@@ -5,7 +5,7 @@ return {
       vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
       vim.keymap.set('n', 'gr', '<cmd>diffget //3<CR>')
       vim.keymap.set('n', 'gl', '<cmd>diffget //2<CR>')
-      vim.keymap.set('n', '<M-g>', '<cmd>0GcLog<CR>')
+      vim.keymap.set('n', '<M-g>', '<cmd>0GcLog<CR>', { desc = 'populate history to quick fix' })
       vim.keymap.set('n', '<M-d>', '<cmd>Gdiffsplit<CR>')
     end,
   },
