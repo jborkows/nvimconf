24fe195a9acb8249ff8018feab69578dad39e20a
jborkows <jacek.borkowskiit@gmail.com>
2024-09-12 20:23:35 +0200

    Remove gotest, fix tsserver name

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -102,7 +102,7 @@ return {
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {
+        ts_ls = {
           settings = {
             typescript = {
               inlayHints = {
